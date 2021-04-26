47358cd2555a4b31a6543ef26df52d4c970eaa04
gwilpiszewski <ZnDn6zJv9sxGBDc>
2021-04-26 13:42:41 +0200

    2.0 Models

--- charity_donation/models.py
+++ charity_donation/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+
+
+class Institution(models.Model):
+    FUNDACJA = 'Fundacja'
+    NGO = 'NGO'
+    ZBIORKA_LOKALNA = 'Zbiórka lokalna'
+    CHOICES = [
+        (FUNDACJA, 'Fundacja'),
+        (NGO, 'NGO'),
+        (ZBIORKA_LOKALNA, 'Zbiórka lokalna'),
+
+    ]
+
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=1028)
+    type = models.CharField(max_length=256, choices=CHOICES, default=FUNDACJA)
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=512)
+    city = models.CharField(max_length=128)
+    zip_cope = models.CharField(max_length=128)
+    pick_up_date = models.DateField(max_length=128)
+    pick_up_time = models.CharField(max_length=128)
+    pick_up_comment = models.CharField
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
