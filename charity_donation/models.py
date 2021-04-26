211e4382c7b9de6a3357c583a8b4d1ef6843a6b9
gwilpiszewski <ZnDn6zJv9sxGBDc>
2021-04-26 18:57:54 +0200

    HTML changes, basic views & models

--- charity_donation/models.py
+++ charity_donation/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=256)
@@ -34,4 +35,3 @@ class Donation(models.Model):
     pick_up_time = models.CharField(max_length=128)
     pick_up_comment = models.CharField
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
--- charity_donation/views.py
+++ charity_donation/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 
 class LandingPageView(View):
     def get(self, request):
-        return render(request, 'index.html')
+        return render(request, 'base.html')
 
 
 class AddDonationView(View):
@@ -22,3 +23,17 @@ class LoginView(View):
 class RegisterView(View):
     def get(self, request):
         return render(request, 'register.html')
+
+    # def post(self, request):
+    # first_name = request.POST.get('name')
+    # last_name = request.POST.get('surname')
+    # email = request.POST.get('email')
+    # password = request.POST.get('password')
+    # password2 = request.POST.get('password2')
+    #
+    # if first_name and last_name and email and password and password2 !="" and '@' in email and password == password2:
+    #     User.objects.create(first_name=first_name,
+    #                         last_name=last_name,
+    #                         email=email,
+    #                         password=password)
+    #     return
